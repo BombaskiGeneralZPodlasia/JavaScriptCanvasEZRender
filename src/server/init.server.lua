5d91bd610d223e8f671e677114ceaf1b9abe8296
fgfbfrj_2022 <62259187+lelidkwhatischouldwritehere@users.noreply.github.com>
2022-09-15 20:59:43 +0100

    a
    a

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,9 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Knit = require(ReplicatedStorage.Packages.Knit)
 
+local Services = script.services
+local LinkReference = {}
+for _,Service in ipairs(Services:GetChildren()) do
+   LinkReference[Service.Name] = require(Service)
+   Service.LinkReference = LinkReference
+end
