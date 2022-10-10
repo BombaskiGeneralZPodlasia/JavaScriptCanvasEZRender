90f08c7ac42c37829f150d3e826c95317634e665
fgfbfrj_2022 <62259187+lelidkwhatischouldwritehere@users.noreply.github.com>
2022-10-10 19:29:02 +0100

    Delete src directory

--- src/server/init.server.lua
+++
@@ -1,9 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Knit = require(ReplicatedStorage.Packages.Knit)
-
-local Services = script.services
-local LinkReference = {}
-for _,Service in ipairs(Services:GetChildren()) do
-   LinkReference[Service.Name] = require(Service)
-   Service.LinkReference = LinkReference
-end
